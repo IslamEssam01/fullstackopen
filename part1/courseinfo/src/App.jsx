06aaef9405a588c3754a7e0da753f0750aed988f
IslamEssam01 <essamislam260105@gmail.com>
2024-06-22 23:07:16 +0300

    made the courseinfo exercise

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,45 @@
+function Header({ course }) {
+    return <h1>{course}</h1>;
+}
+
+function Part({ part }) {
+    return (
+        <p>
+            {part.name} {part.exercises}
+        </p>
+    );
+}
+function Content({ parts }) {
+    return (
+        <>
+            {parts.map((part) => (
+                <Part part={part} key={part.name} />
+            ))}
+        </>
+    );
+}
+
+function Total({ parts }) {
+    const total = parts.reduce((acc, part) => acc + part.exercises, 0);
+    return <p>Number of exercises{total}</p>;
+}
+function App() {
+    const course = {
+        name: "Half Stack application development",
+        parts: [
+            { name: "Fundamentals of React", exercises: 10 },
+            { name: "Using props to pass data", exercises: 7 },
+            { name: "State of a component", exercises: 14 },
+        ],
+    };
+
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    );
+}
+
+export default App;
